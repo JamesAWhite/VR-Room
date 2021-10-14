d9240854b19806bbe5b6cd7ff8935ee049522a4c
BigSweatyHotWing <55860126+BigSweatyHotWing@users.noreply.github.com>
2021-10-13 20:38:53 -0400

    Camera functionality
    Camera now only prints photo when aimed at the proper target

---
+++ Assets/MyRayScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public SpawnObject objectSpawner;
+    RaycastHit hit;
+    public void Cast()
+    {
+        int layerMask = 1 << 3;
+
+        if (Physics.Raycast(this.gameObject.transform.position, this.gameObject.transform.forward, out hit, Mathf.Infinity, layerMask))
+        {
+            if (hit.transform.gameObject.name == "shittydude")
+            {
+                objectSpawner.Spawn();
+            }
+            Debug.Log(hit.transform.gameObject.name);
+        }
+    }
+}
